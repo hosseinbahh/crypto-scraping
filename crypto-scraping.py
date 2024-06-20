6088da24ca5562a0b7c25a2ad5ee4312f2b40b2b
hosseinbahh <149687496+hosseinbahh@users.noreply.github.com>
2024-06-20 15:19:27 +0330

    add scraping 5 crypto of list

---
+++ crypto-scraping.py
@@ -0,0 +1,38 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from urllib.error import URLError
+from time import sleep
+import numpy as np
+
+try:
+    html = urlopen('https://coinranking.com')
+    bs = BeautifulSoup(html.read(), 'lxml')
+    
+    # استخراج تمام ردیف‌های جدول
+    table_rows = bs.find_all('div', {'class': 'valuta'})
+    
+    prices = []
+    
+    # استخراج و تبدیل قیمت‌ها به اعداد شناور
+    for i in range(len(table_rows)):
+        price_string = table_rows[i].get_text().replace('$', '').replace(' ', '').replace('\n', '').replace(',', '')
+        try:
+            price = float(price_string)
+            prices.append(price)
+        except ValueError:
+            continue  # در صورتی که تبدیل به عدد امکان‌پذیر نباشد، ادامه دهید
+    
+    # تقسیم قیمت‌ها به دسته‌های پنج تایی و محاسبه میانگین هر دسته
+    for i in range(0, len(prices), 5):
+        batch = prices[i:i + 5]
+        if batch:
+            average_price = np.mean(batch)
+            print(f"Batch {i // 5 + 1}: {batch}")
+            print(f"Average price: {average_price}")
+            print("******************************************")
+
+except URLError as e:
+    print("you should connect to the internet")
+    sleep(1)
+
+
